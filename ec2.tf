2d52fd58c0231ba9d73d32121010fd3a26192662
AravindReddy91 <113852280+AravindReddy91@users.noreply.github.com>
2023-10-27 21:05:32 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
 # Test SG
-resource "aws_security_group" "ibm_app_sg" {
+resource "aws_security_group" "aquila_app_sg" {
   name = "allow_all"
   description = "Allow all inbound traffic"
-  vpc_id      = aws_vpc.ibm.id
+  vpc_id      = aws_vpc.aquila.id
 
   ingress {
       from_port = 0
@@ -25,12 +25,12 @@ resource "aws_security_group" "ibm_app_sg" {
 
 
 # Launch Instance
-resource "aws_instance" "ibm_instance" {
+resource "aws_instance" "aquila_instance" {
   ami           = "ami-011996ff98de391d1" 
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.ibm_pub_sb.id
-  key_name      = "ravi"
+  subnet_id     = aws_subnet.aquila_pub_sb.id
+  key_name      = "ec"
   count = 2
-  vpc_security_group_ids = [ aws_security_group.ibm_app_sg.id ]
-  user_data = file("ecomm.sh")
+  vpc_security_group_ids = [ aws_security_group.aquila_app_sg.id ]
+  user_data = file("aquila.sh")
 }
